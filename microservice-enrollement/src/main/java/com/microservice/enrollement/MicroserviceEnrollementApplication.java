dbf27bf9152aabaa3bd56d669a24f09f006ba6a0
Martin Zalazar <martinzetav@gmail.com>
2025-04-17 12:12:09 -0300

    se creo el microservicio para las inscripciones

---
+++ microservice-enrollement/src/main/java/com/microservice/enrollement/MicroserviceEnrollementApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.enrollement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEnrollementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEnrollementApplication.class, args);
+	}
+
+}
---
+++ microservice-enrollement/src/test/java/com/microservice/enrollement/MicroserviceEnrollementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.enrollement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEnrollementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
