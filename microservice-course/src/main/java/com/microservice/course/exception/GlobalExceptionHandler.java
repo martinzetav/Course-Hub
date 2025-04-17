aa029ac530266101a0749e467b3ffafbb738ea47
Martin Zalazar <martinzetav@gmail.com>
2025-04-17 12:06:57 -0300

    se agrego excepcion para los datos duplicados

--- microservice-course/src/main/java/com/microservice/course/exception/GlobalExceptionHandler.java
+++ microservice-course/src/main/java/com/microservice/course/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.microservice.course.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,5 +12,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> processResourceNotFoundException(ResourceNotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> processDataIntegrityViolation(DataIntegrityViolationException e){
+        String message = "Data integrity error. Please ensure the data is not duplicated and is valid.";
+        return new ResponseEntity<>(message, HttpStatus.CONFLICT);
     }
 }
